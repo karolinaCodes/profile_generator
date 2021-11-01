595def5522f306d894b3354330eadfee791b4d67
Karolina Swislocki <karolinaswislocki@hotmail.com>
2021-11-01 15:47:35 -0400

    add completed implementation of survey app

---
+++ survey.js
@@ -0,0 +1,44 @@
+const readline = require("readline");
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+rl.question("What's your name? Nicknames are also acceptable :) ", (name) => {
+  rl.question("What's an activity you like doing? ", (activity) => {
+    rl.question("What do you listen to while doing that? ", (musicTaste) => {
+      rl.question(
+        "Which meal is your favourite (eg: dinner, brunch, etc.) ",
+        (favMeal) => {
+          rl.question(
+            "What's your favourite thing to eat for that meal? ",
+            (favFood) => {
+              rl.question(
+                "Which sport is your absolute favourite? ",
+                (favSport) => {
+                  rl.question(
+                    "What is your superpower? In a few words, tell us what you are amazing at! ",
+                    (superpower) => {
+                      console.log(`${name} loves listening to ${musicTaste} while ${activity}, devouring ${favFood} for ${favMeal}, prefers ${favSport} over any other sport, and is amazing at ${superpower}.
+                    `);
+                      rl.close();
+                    }
+                  );
+                }
+              );
+            }
+          );
+        }
+      );
+    });
+  });
+});
+
+/* 
+What's an activity you like doing?
+What do you listen to while doing that?
+Which meal is your favourite (eg: dinner, brunch, etc.)
+What's your favourite thing to eat for that meal?
+Which sport is your absolute favourite?
+What is your superpower? In a few words, tell us what you are amazing at!*/
